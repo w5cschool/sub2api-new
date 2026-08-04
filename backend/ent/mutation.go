1ebe6eed4054a1aea57b92fcaa53ed6fb5fd3514
steve <steve@steves-MBP.lan>
2026-08-04 12:55:14 -0700

    fix: regenerate ent runtime from merged schema

--- backend/ent/mutation.go
+++ backend/ent/mutation.go
@@ -25218,7 +25218,7 @@ func (m *GroupMutation) Type() string {
 // order to get all numeric fields that were incremented/decremented, call
 // AddedFields().
 func (m *GroupMutation) Fields() []string {
-	fields := make([]string, 0, 58)
+	fields := make([]string, 0, 56)
 	if m.created_at != nil {
 		fields = append(fields, group.FieldCreatedAt)
 	}
